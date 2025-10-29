30c4673421923f7c37ac1000caefcc46332c5248
PandesalPanpan <petermarticio@gmail.com>
2025-10-29 12:01:01 +0800

    Feat: Using useEffect to fetch products

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
 import styles from './ShopPage.module.css';
 
 export default function ShopPage() {
+    const [isLoading, setLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        const fetchProduct = async () => {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products', { signal: controller.signal });
+                if (!response.ok) throw new Error('Network error');
+                const json = await response.json();
+                console.log(json);
+                setProducts(json);
+            } catch (err) {
+                if (err.name !== 'AbortError') console.error(err);
+            }
+        }
+
+        fetchProduct();
+
+        return () => controller.abort();
+
+    }, []);
 
     return (
         <div className={styles.pageContainer}>
@@ -8,9 +32,9 @@ export default function ShopPage() {
             {/*Probably make this into a component*/}
             <h2>Bigger Grid here</h2>
             <div className={styles.featuredCollections}>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
+                {products.map((product) => {
+                    return (<li key={product.id}>{product.title}</li>)
+                })}
             </div>
         </div>
     )
