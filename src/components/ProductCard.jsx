e8e7ff295946819af7e335b4a97702997c707778
PandesalPanpan <petermarticio@gmail.com>
2025-10-30 10:43:25 +0800

    Feat: Add stars and number of reviews

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import styles from './ProductCard.module.css';
 import AddToCart from './AddToCart';
+import { StarRating } from 'react-flexible-star-rating';
 export default function ProductCard({
     name,
     image,
@@ -16,7 +17,17 @@ export default function ProductCard({
                 <h3>{name}</h3>
                 <div className={styles.flexPriceStars}>
                     <p>${price}</p>
-                    <span>{rating.rate}</span>
+                    <div className={styles.flexStarsCount}>
+                        <StarRating
+                        starsLength={5}
+                        isHoverEnabled={false}
+                        isReadOnly={true}
+                        initialRating={Math.floor(rating.rate)}
+                        dimension={7}
+                        color="#ffd700"
+                        />
+                        <span>({rating.count})</span>
+                    </div>
                 </div>
             </div>
             <AddToCart></AddToCart>
