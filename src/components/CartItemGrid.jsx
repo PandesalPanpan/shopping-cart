dfb86846a40748288a9c231b25fb342e0c44e455
PandesalPanpan <petermarticio@gmail.com>
2025-11-01 07:40:00 +0800

    Feat: Show a navigate to Shop Page if cart is empty

--- src/components/CartItemGrid.jsx
+++ src/components/CartItemGrid.jsx
@@ -3,15 +3,19 @@ import { CartItemsContext } from '../providers/CartItemsProvider';
 import CartItem from './CartItem';
 import styles from './CartItemGrid.module.css';
 import Checkout from './Checkout';
+import { Link } from 'react-router';
 
 export default function CartItemGrid() {
     const cartItems = useContext(CartItemsContext);
 
     if (cartItems.length < 1) {
         return (
-            <h2>
-                You have an empty cart.
-            </h2>
+            <div className={styles.textCenter}>
+                <h2>
+                    You have an empty cart.
+                </h2>
+                <h3>Navigate to <Link to="shop">Shop Page</Link></h3>
+            </div>
         )
     }
 
