a7e10b4ff9b955a78bce4d34fd6911c3ac333399
PandesalPanpan <petermarticio@gmail.com>
2025-10-31 10:47:04 +0800

    WIP: Cart Item Component

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,16 @@
+export default function CartItem({
+    name,
+    imageURL,
+    price,
+    count
+}) {
+    // Calculate the price total in here
+
+    // Add a dispatch on adding and removing buttons
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
