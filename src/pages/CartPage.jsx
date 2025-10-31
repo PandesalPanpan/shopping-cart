04f6005dd8f226cb91b7b61b056d449cb7770942
PandesalPanpan <petermarticio@gmail.com>
2025-10-31 10:43:12 +0800

    Feat: Simple display for cart items in CartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,17 +1,25 @@
+import { useContext } from 'react';
 import PageHeader from '../components/PageHeader'
+import { CartItemsContext } from '../providers/CartItemsProvider'
 import styles from './CartPage.module.css'
 
 export default function CartPage() {
+    const cartItems = useContext(CartItemsContext);
 
     return (
         <div className={styles.pageContainer}>
             <PageHeader>Cart Page</PageHeader>
-            {/*Probably make this into a component*/}
+            {cartItems.map((item) => {
+                console.log(item);
+                return (
+                    <div>
+                        <p>{item.id}</p>
+                        <p>{item.name}</p>
+                        <p>{item.count}</p>
+                        <p>{item.price}</p>
+                    </div>
+                )
+            })}
             <h2>Linear Grid with a Checkout and Total</h2>
-            <div className={styles.featuredCollections}>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </div>
         </div>)
 }
