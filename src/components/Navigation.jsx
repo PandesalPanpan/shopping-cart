b3cfb7fc6726a1cf257bfd51105109957789f3df
PandesalPanpan <petermarticio@gmail.com>
2025-11-01 07:30:36 +0800

    Feat: Add Item count badge on cart navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react';
 import styles from './Navigation.module.css';
 import { Link } from 'react-router';
+import { CartItemsContext } from '../providers/CartItemsProvider';
+import { getTotalItemsCount } from '../utils/cart-data-utils';
 
 export default function Navigation() {
+    const cartItems = useContext(CartItemsContext);
+    const totalItemsCount = getTotalItemsCount(cartItems);
 
     return (
         <nav className={styles.navigationContainer}>
             <ul className={styles.navigationList}>
                 <li><Link className={styles.navLink}to="/">Home</Link></li>
                 <li><Link className={styles.navLink}to="shop">Shop</Link></li>
-                <li><Link className={styles.navLink}to="cart">Cart</Link></li>
+                <li><Link className={styles.navLink}to="cart">Cart
+                {totalItemsCount > 0 ? <span className={styles.itemCountBadge}>{totalItemsCount}</span> : null}
+                </Link></li>
             </ul>
         </nav>
     )
