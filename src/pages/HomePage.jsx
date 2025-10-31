32c6e37bc54940c2235c2daedc1d3183139ea455
PandesalPanpan <petermarticio@gmail.com>
2025-11-01 07:38:04 +0800

    Feat: Homepage displays summary

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import PageHeader from '../components/PageHeader';
 import styles from './HomePage.module.css';
 
@@ -6,12 +7,14 @@ export default function LandingPage() {
     return (
         <div className={styles.pageContainer}>
             <PageHeader>Home Page</PageHeader>
-            {/*Probably make this into a component*/}
-            <div className={styles.featuredCollections}>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </div>
+            <h2>Shopping Cart Application</h2>
+            <ul>
+                <li>A React with the React 19 Compiler</li>
+                <li>Client-Side Routing with React Router</li>
+                <li>Uses useContext and useReducer for Cart Logic</li>
+                <li>Has Skeleton Loading for Shop Product Items</li>
+                <li>And many more, try out in <Link to="shop">Shop</Link> and <Link to="cart">Cart</Link> Page</li>
+            </ul>
         </div>
     )
 }
